d3f9a561fb9dd619c95d848191e7d674d5ef353b
nkakalov <nkakalov>
2021-01-26 17:51:11 +0300

    save1Task2

---
+++ charOut.js
@@ -0,0 +1,16 @@
+let string = 'Я изучаю JavaScript'
+let rep1 = /JavaScript/
+let rep2 = /ю/
+
+let newstr2 = string.replace(rep2,'G')
+let newstr1 = string.replace(rep1,'Python')
+console.log(newstr1)
+console.log(newstr2) 
+
+
+/*
+str = 'Я изучаю JavaScript'.split("");
+str[7] = 'M';
+str = str.join("");
+alert(str);
+*/
